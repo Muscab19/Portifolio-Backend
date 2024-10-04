37525b74bf58bc916d023e9a7c1a32c1cf83dea0
Muscab19 <muscabmaxamuud356@gmail.com>
2024-10-04 13:02:27 -0700

    Database has been conected

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection
-mongoose.connect("mongodb://localhost:27017/Portifolio")
+mongoose.connect("mongodb+srv://muscabmaxamuud356:6JOcFRTUnvFfyd8S@cluster0.v2z8p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => {
         console.log("Database has been connected successfully");
     })
